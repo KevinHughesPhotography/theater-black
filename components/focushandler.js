878cff6adbdf7247f8a76990d2c95e3c78ab31bb
chefbennyj1 <chefbennyj@gmail.com>
2017-08-08 09:16:51 -0400

    Update focushandler.js

--- components/focushandler.js
+++ components/focushandler.js
@@ -4,6 +4,23 @@ define(['imageLoader', 'itemHelper', 'backdrop', 'mediaInfo', 'focusManager', 's
 
         var enableAnimations = browser.animate || browser.edge;
 
+    function vanishIn(elem){
+         var keyframes = [
+                { 
+                 opacity: '0',
+                 transform: 'scale(1, 0.8)',
+                 filter: 'blur(90px)'
+                },
+                {
+                opacity: '1',
+                transform: 'scale(1, 1)',
+                filter: 'blur(0px)'
+                }];
+        var timing = { duration: 1000, iterations: 1 };
+        return elem.animate(keyframes, timing);
+    }
+    
+    
         function fadeIn(elem) {
 
             var keyframes = [
